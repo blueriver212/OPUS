6d877c15752d6f9b4e756b4d2f559e61218c78e5
sgraff1412 <smgraff@ncsu.edu>
2025-02-26 14:08:27 -0500

    Update basic_adr.py
    remove lfs

--- OPUS/utils/basic_adr.py
+++ OPUS/utils/basic_adr.py
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:46b72cd9854766e8a55e44e1218ed80ad169a2c2c6bc35a33e237cadd1d23d90
-size 205
+import numpy as np
+
+
+# attempting to write a lil script
+
+def implement_adr(time_idx, times, shell, percentage, species_name):
+    testing = "value"
+    # if ((time_idx in times) and (species_name =)
