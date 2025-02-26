88db1a9beb41c5c1c1c0a54739153d0837e18205
sgraff14 <smgraff@ncsu.edu>
2025-02-26 11:30:07 -0500

    this one is just for me pretend it works perfectly

--- OPUS/main.py
+++ OPUS/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ec64bb72765904a042478edf6e8d5ecbedbd06eeee62f88978a6a88649860b9
-size 11075
+oid sha256:2a8bd7ec3985c7f0b9bdf2662d3af4509e6e71dcb14f0e02b01f5655d1edddf7
+size 11235
--- OPUS/utils/OpenAccessSolver.py
+++ OPUS/utils/OpenAccessSolver.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0beca10bc0af9bbacbe82cd7d614ad5f8d91ffc41216382e3fb651fd70259e92
-size 6092
+oid sha256:c995e763bcfca8f3bfc64c9f2b7ee58c25b44d55f43c9b4cecd231db7d7017e8
+size 6155
