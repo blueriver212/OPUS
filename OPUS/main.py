3ed80e89204ef660a68b7952f532fdff143efc0b
sgraff14 <smgraff@ncsu.edu>
2025-02-26 13:46:12 -0500

    i think i finally got rid of lfs stuff??? it was causing me problems

--- OPUS/main.py
+++ OPUS/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23e72d2a5f766c51687c75bf3b09f65919c4e7ac7078d642fbc26ca5eb7d489a
-size 1653
+oid sha256:0689a374b06af2721e60c24073922bd9b224e8d61080ef6781e5f743f6a631aa
+size 14091
