184e4a4efbbc41ce54d68403db7d0578259246c0
Indigo Brownhall <indy.brownhall@gmail.com>
2025-04-30 18:33:24 +0100

    adding the framework for multi-species opus

--- OPUS/utils/MocatParameters.py
+++ OPUS/utils/MocatParameters.py
@@ -1,8 +1,10 @@
 from pyssem.model import Model # mocat-ssem
+from .MultiSpecies import MultiSpecies
+from .EconParameters import EconParameters
 import json 
 import numpy as np
 
-def configure_mocat(MOCAT_config: json, fringe_satellite: str = None) -> Model:
+def configure_mocat(MOCAT_config: json, multi_species: MultiSpecies) -> Model:
     """
         Configure's MOCAT-pySSEM model with a provided input json. 
         To find a correct configuration, please refer to the MOCAT documentation. https://github.com/ARCLab-MIT/pyssem/
@@ -14,11 +16,6 @@ def configure_mocat(MOCAT_config: json, fringe_satellite: str = None) -> Model:
             Model: An configured instance of the MOCAT model.
     """
     scenario_props = MOCAT_config["scenario_properties"]
-    try:
-        economic_parameters_json = MOCAT_config['OPUS']
-    except:
-        raise ValueError("Please ensure that the economic parameters of OPUS are configured correctly.")
-
     # Create an instance of the pySSEM_model with the simulation parameters
     MOCAT = Model(
         start_date=scenario_props["start_date"].split("T")[0],  # Assuming the date is in ISO format
@@ -43,23 +40,46 @@ def configure_mocat(MOCAT_config: json, fringe_satellite: str = None) -> Model:
     species = MOCAT_config["species"]
 
     MOCAT.configure_species(species)
-    MOCAT.opus_active_loss_setup(fringe_satellite)
+    # Create an active_loss_setup for each of the species in the model.
+    for species in multi_species.species:
+        MOCAT.opus_active_loss_setup(species.name)
+
     MOCAT.initial_population() 
     MOCAT.build_model()
 
     print("You have these species in the model: ", MOCAT.scenario_properties.species_names)
 
     # Find the PMD linked species and return the index. 
-    pmd_linked_species_to_fringe = [
-        species
-        for species_group in MOCAT.scenario_properties.species.values()
-        for species in species_group
-        if any(linked_species.sym_name == fringe_satellite for linked_species in species.pmd_linked_species)
-    ]
+    for opus_species in multi_species.species:
+        pmd_linked_species_to_fringe = [
+            species
+            for species_group in MOCAT.scenario_properties.species.values()
+            for species in species_group
+            if any(linked_species.sym_name == opus_species.name for linked_species in species.pmd_linked_species)
+        ]
 
-    if len(pmd_linked_species_to_fringe) != 1:
-        raise ValueError("Please ensure that there is only one species linked to the fringe satellite.")
-    else:
-        pmd_linked_species = pmd_linked_species_to_fringe[0].sym_name
+        if len(pmd_linked_species_to_fringe) != 1:
+            raise ValueError("Please ensure that there is only one species linked to the fringe satellite.")
+        else:
+            opus_species.pmd_linked_species = pmd_linked_species_to_fringe[0].sym_name
+
+    # Match the econ parameters from the json to the multispecies object.
+    for dict in MOCAT_config["species"]:
+        try:
+            opus_params = dict['OPUS']
+            sym_name = dict['sym_name']
+            for species in multi_species.species:
+                if species.name == sym_name:
+                    species.econ_params = opus_params
+        except KeyError:
+            # If the sym_name is in the multi_species object, it means it has been asked to be econ parameterized. Used the default values.
+            for obj in multi_species.species:
+                if obj.name == dict['sym_name']:
+                    obj.econ_params = EconParameters({}, MOCAT)
+                    print("The species: ", dict['sym_name'], " is not econ parameterized. Using the default values.")
+                    break
+            print("Using the default economic parameters for the species: ", dict['sym_name'])
+            print(f"Key 'OPUS' not found in the dictionary for species '{dict['sym_name']} \n Please include if you want to use the economic parameters in the model.")
+
  
-    return MOCAT, economic_parameters_json, pmd_linked_species
+    return MOCAT, multi_species
